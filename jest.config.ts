e412e6ccd62d74d0fb7caaf8fcb8bdf48e2231ed
ama-leanix <amine.abdelhedi@leanix.net>
2026-02-04 17:41:32 +0100

    CMP-212 Fix interactive test cases in CI
    and lint create-lxr index.spec.ts & jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,34 +1,38 @@
-import type { Config } from '@jest/types'
+import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   verbose: true,
-  testEnvironment: 'node',
+  testEnvironment: "node",
   transform: {
-    '^.+\\.m?[tj]sx?$': [
-      'babel-jest',
+    "^.+\\.m?[tj]sx?$": [
+      "babel-jest",
       {
-        tsconfig: '<rootDir>/tsconfig.json'
-      }
-    ]
+        tsconfig: "<rootDir>/tsconfig.json",
+      },
+    ],
   },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/packages/create-lxr/src/test-app",
+    "<rootDir>/packages/create-lxr/dist",
+  ],
   projects: [
     {
-      displayName: '@lxr/core',
-      testMatch: ['<rootDir>/packages/core/src/**/*.spec.ts'],
+      displayName: "@lxr/core",
+      testMatch: ["<rootDir>/packages/core/src/**/*.spec.ts"],
       // TODO: // line 12 => https://stackoverflow.com/questions/69383514/node-fetch-3-0-0-and-jest-gives-syntaxerror-cannot-use-import-statement-outside
-      transformIgnorePatterns: []
+      transformIgnorePatterns: [],
     },
     {
-      displayName: 'vite-plugin-lxr',
-      testMatch: ['<rootDir>/packages/vite-plugin-lxr/src/**/*.spec.ts']
+      displayName: "vite-plugin-lxr",
+      testMatch: ["<rootDir>/packages/vite-plugin-lxr/src/**/*.spec.ts"],
     },
     {
-      displayName: 'create-lxr',
-      testMatch: ['<rootDir>/packages/create-lxr/src/**/*.spec.ts'],
+      displayName: "create-lxr",
+      testMatch: ["<rootDir>/packages/create-lxr/src/**/*.spec.ts"],
       // TODO: // line 12 => https://stackoverflow.com/questions/69383514/node-fetch-3-0-0-and-jest-gives-syntaxerror-cannot-use-import-statement-outside
-      transformIgnorePatterns: []
-    }
-  ]
-}
-export default config
+      transformIgnorePatterns: [],
+    },
+  ],
+};
+export default config;
