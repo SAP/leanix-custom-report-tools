bce38d8126c2d075aa94a6886c9d6dd0b8039c5f
ama-leanix <amine.abdelhedi@leanix.net>
2026-02-24 12:16:55 +0100

    CMP-259 Replace `ESLint` stylistic with `Prettier`

--- packages/create-custom-report/templates/react-ts/eslint.config.mjs
+++ packages/create-custom-report/templates/react-ts/eslint.config.mjs
@@ -4,14 +4,8 @@ export default antfu({
   lessOpinionated: true,
   react: true,
   typescript: true,
+  stylistic: false,
   rules: {
     'no-console': 'warn'
-  },
-  stylistic: {
-    overrides: {
-      'style/comma-dangle': ['error', 'always-multiline'],
-      'style/semi': ['error', 'always'],
-      'style/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }]
-    }
   }
 });
